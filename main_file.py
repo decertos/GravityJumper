d1ed2e6af2a282234b7db95246facb8abb11a0aa
decert <dyadenkodmitriy1@yandex.ru>
2025-01-25 21:11:24 +0400

    Added standard platform and jumping

--- main_file.py
+++ main_file.py
@@ -1,6 +1,6 @@
 from python_files.variables import *
 from collections import deque
-from random import choices
+from random import choices, randint
 from PIL import Image
 from time import time
 import pygame
@@ -13,23 +13,40 @@ class Player(pygame.sprite.Sprite):
 
         # Generating frames
         self.frames = []
+        self.reversed_frames = []
         for file_name in os.listdir("assets/frames"):
             if "lizard_m" in file_name:
                 self.frames.append(pygame.image.load(f"assets/frames/{file_name}"))
+                self.reversed_frames.append(pygame.image.load(f"assets/frames/{file_name}"))
+
                 image_width, image_height = self.frames[-1].get_width(), self.frames[-1].get_height()
                 self.frames[-1] = pygame.transform.scale(self.frames[-1],
                                                          (image_width * ENLARGING_COEFFICIENT,
                                                           image_height * ENLARGING_COEFFICIENT))
+
+                self.reversed_frames[-1] = pygame.transform.scale(self.reversed_frames[-1],
+                                                                  (image_width * ENLARGING_COEFFICIENT,
+                                                                   image_height * ENLARGING_COEFFICIENT))
+
+                self.reversed_frames[-1] = pygame.transform.flip(self.reversed_frames[-1], False, True)
                 self.frames[-1].set_colorkey((0, 0, 0))
+                self.reversed_frames[-1].set_colorkey((0, 0, 0))
 
         # Setting positions
         self.image = self.frames[0]
         self.current_image_index = 0
-        self.rect = pygame.Rect(10, SCREEN_HEIGHT - self.image.get_height() - 5, self.image.get_width(), self.image.get_height())
+        self.rect = pygame.Rect(20, SCREEN_HEIGHT - self.image.get_height() - 15, self.image.get_width(),
+                                self.image.get_height())
+        self.vx, self.vy = 0, 0
+        self.up_pos = False
 
         # Animation delay
         self.animation_delta = PLAYER_ANIMATION_DELTA
         self.prev_time = -1
+
+        self.collided = False
+        self.flown = False
+
 
     def change_image(self):
         if time() - self.prev_time < self.animation_delta:
@@ -38,11 +55,63 @@ class Player(pygame.sprite.Sprite):
         self.prev_time = time()
         # Animation
         self.current_image_index = (self.current_image_index + 1) % len(self.frames)
-        self.image = self.frames[self.current_image_index]
+        if self.up_pos:
+            self.image = self.reversed_frames[self.current_image_index]
+        else:
+            self.image = self.frames[self.current_image_index]
         self.rect = pygame.Rect(self.rect.x, self.rect.y, self.image.get_width(), self.image.get_height())
 
-    def move(self, x, y):
-        self.rect = self.rect.move(x, y)
+    def move(self):
+        if self.vy != 0 and not self.flown:
+            max_pos = TILE_HEIGHT * 3 if self.vy < 0 \
+                else SCREEN_HEIGHT - self.rect.height - 20
+            if self.vy > 0 and self.rect.y > max_pos:
+                self.rect.y = max_pos
+                self.vy = 0
+            elif self.vy < 0 and self.rect.y < max_pos:
+                self.rect.y = max_pos
+                self.vy = 0
+        if self.vy != 0:
+            self.vy += self.vy // abs(self.vy)
+        self.rect = self.rect.move(self.vx, self.vy)
+        if self.rect.y > SCREEN_HEIGHT or self.rect.y < 0 - self.rect.height:
+            exit(0)
+
+    def tiles_check(self):
+        collided = False
+        for tile in tiles_up:
+            if not tile.rect.colliderect(self.rect):
+                continue
+            collided = True
+            if isinstance(tile, ElectricalTile):
+                tile_timer = tile.timer
+                if tile_timer >= 5:
+                    exit(0)
+
+        collided1 = False
+        for tile in tiles_down:
+            if not tile.rect.colliderect(self.rect):
+                continue
+            collided = True
+            if isinstance(tile, ElectricalTile):
+                tile_timer = tile.timer
+                if tile_timer >= 5:
+                    exit(0)
+
+        if self.vy == 0 and not collided and not collided1:
+            self.vy = -1 if self.up_pos == True else 1
+            self.vy *= 5
+            self.flown = True
+
+    def reverse_jump(self):
+        if self.up_pos:
+            self.image = self.frames[self.current_image_index]
+            self.up_pos = not self.up_pos
+            self.vy = 1
+            return
+        self.image = self.reversed_frames[self.current_image_index]
+        self.up_pos = not self.up_pos
+        self.vy = -1
 
 
 class Tile(pygame.sprite.Sprite):
@@ -99,6 +168,14 @@ class ElectricalTile(Tile):
         self.rect = self.rect.move(x, y)
 
 
+class NormalTile(Tile):
+    def __init__(self, position):
+        frames = [pygame.transform.scale(pygame.image.load("assets/tiles_animations/normal/1.png"),
+                                         (TILE_WIDTH * ENLARGING_COEFFICIENT,
+                                          TILE_HEIGHT * ENLARGING_COEFFICIENT))]
+        super().__init__(position, frames)
+
+
 def get_image(file, position, size, new_file_name):
     # Crop images
     tile_image = Image.open(file)
@@ -110,7 +187,8 @@ def get_image(file, position, size, new_file_name):
 def reload_images():
     # Wall images
     for i in range(WALL_IMAGES_COUNT):
-        get_image("assets/images/all_walls.png", (i * WALL_WIDTH + i, 0), (WALL_WIDTH, WALL_HEIGHT), f"assets/images/wall_images/wall{i}.png")
+        get_image("assets/images/all_walls.png", (i * WALL_WIDTH + i, 0), (WALL_WIDTH, WALL_HEIGHT),
+                  f"assets/images/wall_images/wall{i}.png")
 
 
 if __name__ == "__main__":
@@ -131,20 +209,25 @@ if __name__ == "__main__":
     player = Player()
     player_sprite.add(player)
 
-    tiles = deque()
+    tiles_up = deque()
+    tiles_down = deque()
 
-    ALL_TILES = [ElectricalTile]
+    ALL_TILES = [NormalTile, ElectricalTile]
 
     # Technical things
     clock = pygame.time.Clock()
     wall_render_delta = 0
+    score = 0
 
     # Game loop
     running = True
-    current_wall_images = deque(choices(wall_images, k=3) + choices(wall_images, k=3))
-    for tile in [ElectricalTile(((i * WALL_IMAGE_WIDTH + WALL_IMAGE_WIDTH // 2 - TILE_WIDTH,
-                                                    SCREEN_HEIGHT - TILE_HEIGHT * 2))) for i in range(6)]:
-        tiles.append(tile)
+    current_wall_images = deque(choices(wall_images, k=4))
+    for i, tile in enumerate([choices(ALL_TILES, k=1)[0] for i in range(4)]):
+        new_tile = tile
+        tiles_up.append(new_tile((i * WALL_IMAGE_WIDTH + WALL_IMAGE_WIDTH // 2 - TILE_WIDTH,
+                                  SCREEN_HEIGHT - TILE_HEIGHT * 2)))
+        tiles_down.append(new_tile((i * WALL_IMAGE_WIDTH + WALL_IMAGE_WIDTH // 2 - TILE_WIDTH,
+                                    TILE_HEIGHT * 2)))
 
     while running:
         screen.fill("black")
@@ -152,30 +235,58 @@ if __name__ == "__main__":
             if event.type == pygame.QUIT:
                 running = False
                 break
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                player.reverse_jump()
 
         # Drawing
         for i in range(len(current_wall_images)):
             screen.blit(current_wall_images[i], (i * WALL_IMAGE_WIDTH - wall_render_delta, 0))
-
-        player_sprite.draw(screen)
-
-        for i in tiles:
-            i.move(-WALLS_SPEED, 0)
+        for i in tiles_up:
+            i.rect.x -= WALLS_SPEED
             i.change_image()
             i.draw()
+        for i in tiles_down:
+            i.rect.x -= WALLS_SPEED
+            i.change_image()
+            i.draw()
+
+        player_sprite.draw(screen)
+        player.move()
+        player.tiles_check()
 
         # Walls rendering
         wall_render_delta += WALLS_SPEED
         if wall_render_delta >= WALL_IMAGE_WIDTH:
             current_wall_images.popleft()
             current_wall_images.append(choices(wall_images, k=1)[0])
-            tiles.popleft()
-            tiles.append(choices(ALL_TILES, k=1)[0]((3 * WALL_IMAGE_WIDTH + WALL_IMAGE_WIDTH // 2 - TILE_WIDTH,
-                                                    SCREEN_HEIGHT - TILE_HEIGHT * 2)))
+            tiles_up.popleft()
+            tiles_down.popleft()
+            up = choices(ALL_TILES, k=1)[0]((3 * WALL_IMAGE_WIDTH + WALL_IMAGE_WIDTH // 2 - TILE_WIDTH,
+                                                        TILE_HEIGHT * 2))
+            down = choices(ALL_TILES, k=1)[0]((3 * WALL_IMAGE_WIDTH + WALL_IMAGE_WIDTH // 2 - TILE_WIDTH,
+                                                        SCREEN_HEIGHT - TILE_HEIGHT * 2))
+            if isinstance(up, ElectricalTile) and isinstance(down, ElectricalTile):
+                number = randint(1, 2)
+                if number == 1:
+                    up = NormalTile((3 * WALL_IMAGE_WIDTH + WALL_IMAGE_WIDTH // 2 - TILE_WIDTH,
+                                                        TILE_HEIGHT * 2))
+                elif number == 2:
+                    down = NormalTile((3 * WALL_IMAGE_WIDTH + WALL_IMAGE_WIDTH // 2 - TILE_WIDTH,
+                                                        SCREEN_HEIGHT - TILE_HEIGHT * 2))
+            tiles_up.append(up)
+            tiles_down.append(down)
+
+            score += 1
             wall_render_delta = 0
 
         # Animation
         player.change_image()
+
+        # Score
+        font = pygame.font.Font("assets/fonts/ByteBounce.ttf", 32)
+        rendered = font.render(f"Score: {score}", True, (255, 255, 255))
+        screen.blit(rendered, (SCREEN_WIDTH - rendered.get_width() - 5 * ENLARGING_COEFFICIENT,
+                               rendered.get_height() - 5 * ENLARGING_COEFFICIENT))
 
         # Display drawing
         clock.tick(FPS)
